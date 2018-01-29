2ad679e869e8825cf8dcca3db56d47c81ee3df7a
Wojciech Łojkowski <w.lojkow@gmail.com>
2018-01-29 18:47:58 +0100

    Add project files.

---
+++ Vidly_ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_ASP.Startup))]
+namespace Vidly_ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
